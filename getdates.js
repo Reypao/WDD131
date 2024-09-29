c59d7b31dea2c0df8b9c0110684add681684752f
Effrey Guzman <155923368+Reypao@users.noreply.github.com>
2024-09-28 22:34:10 -0600

    Update getdates.js

--- getdates.js
+++ getdates.js
@@ -5,3 +5,12 @@ document.getElementById("currentyear").textContent = currentYear;
 const lastModifiedDate = document.lastModified;
 
 document.getElementById("lastModified").textContent = `Last Modification: ${lastModifiedDate}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+
+});
